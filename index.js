9e406100952c2c964d0e4442ea12f6c224243957
yashu <yashu.tanna@outlook.com>
2018-08-04 00:58:36 +0200

    updated scrips

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const getLastPrice = (cur) => new Promise((resolve, reject) => {
   bfxRest.ticker(`${cur.toUpperCase()}USD`, (err, res) => {
     if (err) reject("Cooling down");
     if (!res || !res.last_price){
-      reject("Cooling down")
+      reject("res not formed correctly: res: \n" + JSON.stringify(res));
     }
     else {
       resolve({
@@ -61,6 +61,9 @@ const getZARToUSDRate = () => {
   return new Promise((res, rej) => {
     request.get(`https://api.fixer.io/latest?base=USD&symbols=ZAR`,
       function(error, response, body) {
+        if(error){
+          rej(error);
+        }
         res(Number(JSON.parse(body).rates.ZAR))
       }
     );
@@ -75,16 +78,19 @@ const getTotalHolding = (holdings, cur) => {
 
 const savePositions = (holdingsValue) =>{
   fs.renameSync('last.json', `${Date.now()}.json`, function(err) {
-      if (err) {
-          console.log('ERROR: ' + err);
-          throw err;
-      }
-      console.log('File renamed!!');
+    if (err) {
+      console.log('ERROR: ' + err);
+      throw err;
+    }
+    console.log('File renamed!!');
   });
   fs.writeFileSync("last.json", JSON.stringify(
     holdingsValue
       .reduce((res, obj) => {
-        res[obj.currency] = Number(obj.value);
+        res[obj.currency] = {
+          value: Number(obj.value),
+          holding: Number(obj.holding)
+        };
         return res;
       }, {})
   ), function(err) {
@@ -112,18 +118,23 @@ const displayHoldings = () =>
             return {...result, price: result.price.toFixed(DECIMAL_POINTS), value: holdingPrice.toFixed(DECIMAL_POINTS), holding: holdings[result.cur]}
           });
           lastResult.forEach(res => {
-            if(res.value < previousPosition[res.cur.toLowerCase()]){
-              console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`.red)
+            if(!!previousPosition[res.cur.toLowerCase()]){
+              if(res.value < previousPosition[res.cur.toLowerCase()].value){
+                console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`["red"])
+              }
+              else if (res.value > previousPosition[res.cur.toLowerCase()].value){
+                console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`.green)
+              }
+              else if (res.value == previousPosition[res.cur.toLowerCase()].value){
+                console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`.blue)
+              }
             }
-            else if (res.value > previousPosition[res.cur.toLowerCase()]){
-              console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`.green)
-            }
-            else if (res.value == previousPosition[res.cur.toLowerCase()]){
-              console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`.blue)
+            else{
+              console.log(`${res.value} \t ${res.cur.toUpperCase()} @ \tUSD ${res.price} \t x ${res.holding.toFixed(DECIMAL_POINTS)}`)
             }
           })
           lastResult.forEach(res => holdingsValue.push({
-            currency: res.cur, value: res.value
+            currency: res.cur, value: res.value, holding: Number(res.holding).toFixed(DECIMAL_POINTS)
           }));
         console.log('===========================================')
         console.log('TOTAL: \t\tUSD ' + totalHoldings.toFixed(DECIMAL_POINTS))
@@ -132,10 +143,18 @@ const displayHoldings = () =>
           return getZARToUSDRate().then(rate => {
             const ZARHolding = (totalHoldings + Number(res[0].amount)) * rate;
             console.log('\t\tZAR ' + ZARHolding.toFixed(DECIMAL_POINTS) + ' @ ' + rate)
-            return savePositions(holdingsValue);
+            return new Promise((resolve, reject) => {
+              try{
+                savePositions(holdingsValue);
+                resolve({ success: true })
+              }
+              catch(err){
+                reject(err)
+              }
+            })
           }).catch(Promise.reject)
         }).catch(Promise.reject)
-      }).catch(Promise.reject)
+      }).catch(console.log)
   }).catch(Promise.reject)
 
 const sell = (cur, units, price) => new Promise((resolve, reject) => {
@@ -148,6 +167,56 @@ const sell = (cur, units, price) => new Promise((resolve, reject) => {
     resolve(res);
   })
 })
+
+const getHoldingsWithCurrentPricesWithoutSide = () => {
+  const allPositions = [];
+  for (var currency in previousPosition){
+    allPositions.push({ currency, ...previousPosition[currency] })
+  }
+
+  return Promise.all(allPositions
+    .filter(position => !(position.currency in otherHoldings))
+    .map(position => getLastPrice(position.currency)
+      .then(priceCouple => {
+        return { ...position, price: priceCouple.price }
+      })
+    )).then(positions => positions.map(position => ({
+        symbol: `${position.currency.toUpperCase()}USD`,
+        amount: position.holding,
+        exchange: 'bitfinnex',
+        type: 'exchange limit',
+        price: (position.price * 0.995).toFixed(5)
+      }))
+    )
+}
+
+const writeJsonToFile = (filename, object) => {
+  fs.writeFileSync(filename, JSON.stringify(object), function(err) {
+      if(err) {
+          console.log(err);
+          return(err);
+      }
+  });
+}
+
+const sellAll = () => getHoldingsWithCurrentPricesWithoutSide().then(orders => {
+  writeJsonToFile("lastSold.json", orders);
+  // var payload = {
+  //    request: '/v1/order/new/multi',
+  //    nonce: Date.now().toString(),
+  //    orders: orders
+  // }
+  // bfxRest.multiple_new_orders(orders, (err, res) => {
+  //   if (err) console.log(err)
+  //   console.log(result)
+  // })
+}).catch(console.log)
+
+const reBuyAll = () => {
+    const previousSellOrder = require("./lastSold.json");
+    const newBuyOrder = previousSellOrder.map(position => ({ ...position, side: 'buy', price: (position.price * 1.005).toFixed(5) }));
+    console.log(newBuyOrder);
+}
 
 const buy = (cur, units, price) => new Promise((resolve, reject) => {
   bfxRest.new_order(cur.toLowerCase() + 'usd', units, price, 'bitfinex', 'buy', 'exchange limit', (err, res) => {
@@ -201,6 +270,8 @@ list
   .option('display orders', 'orders')
   .option('buy currency', 'buy')
   .option('sell currency', 'sell')
+  .option('sell all', 'sellAll')
+  .option('rebuy all', 'rebuyAll')
   .option('cancel order', 'cancel')
   .option('exit')
   .list();
@@ -260,6 +331,12 @@ list.on('select', function(options){
       }
       case 'orders': {
         return activeOrders().then(resolve).catch(reject);
+      }
+      case 'sellAll': {
+        return sellAll().then(resolve).catch(console.log);
+      }
+      case 'rebuyAll': {
+        return reBuyAll();
       }
       case 'cancel': {
         return inquirer.prompt([
